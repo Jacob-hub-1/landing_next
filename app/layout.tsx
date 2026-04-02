4468e7247da8418e42cb95e464a606a6491733f7
aju-alen <alenaju1304@gmail.com>
2026-04-02 15:51:46 +0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,29 +81,15 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      {
-        url: '/al_egaby_logo.svg',
-        type: 'image/svg+xml',
-        sizes: 'any',
-      },
-      {
-        url: '/favicon-48x48.png',
-        type: 'image/png',
-        sizes: '48x48',
-      },
-      {
-        url: '/favicon-96x96.png',
-        type: 'image/png',
-        sizes: '96x96',
-      },
-      {
-        url: '/favicon-192x192.png',
-        type: 'image/png',
-        sizes: '192x192',
-      },
+      { url: '/favicon.ico', sizes: '48x48' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
     ],
     apple: '/apple-touch-icon.png',
   },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
